d30ae8e763fd2313692a6977c5ad441e837c6573
Mantas <bemze007@yahoo.com>
2020-11-29 17:48:47 +0200

    Patikrai

--- js/components/renderSocials.js
+++ js/components/renderSocials.js
@@ -1,37 +1,17 @@
 function renderSocials(data) {
-    //input validation
-    // if (!Array.isArray(data))
-    
-    
-    //logic
-    const socialsDOM = document.querySelector('footer > .row');
     let HTML = '';
 
     for (let i = 0; i < data.length; i++) {
         const item = data[i];
-        if (typeof item !== 'object') {
-            continue;
-        }
-        if (typeof item.link !== "string") {
-            continue;
-        }
-        if (typeof item.icon !== "string" || 
-        item.icon === '') {
-            continue;
-        }
-
 
       HTML +=`<a href="${item.link}" target="_blank" class="fa ${item.icon}" aria-hidden="true"></a>`;
     }
     
-    // post logic validation
-    if (HTML === '') {
-        console.error("Error: nepavyko sugeneruoti nuotodu");
-    }
+    const socialsDOM = document.querySelector('footer > .row');
+    console.log(socialsDOM);
 
-
-    // return
     socialsDOM.innerHTML = HTML;
+    
     
 }
 
