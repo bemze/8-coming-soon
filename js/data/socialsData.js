a860986e166ba6ad04958459d28df78a67741a6f
Mantas <bemze007@yahoo.com>
2020-11-29 17:37:04 +0200

    validation in progress

---
+++ js/data/socialsData.js
@@ -0,0 +1,37 @@
+const socials = [
+    // {
+    //     icon: 'facebook',
+    //     link: '#'
+    // },
+    // 544545,
+    // "eeee",
+    // true,
+    // {},
+    // {
+    //     icon: 'twitter',
+    //     link: '#'
+    // },
+    // {
+    //     icon: 'dribbble',
+    //     link: '#'
+    // },
+    // {
+    //     icon: 'tumblr',
+    //     link: '#'
+    // },
+    // {
+    //     icon: 'linkedin',
+    //     link: '#'
+    // },
+    // {
+    //     icon: 'instagram',
+    //     link: '#'
+    // },
+    // {
+
+    //     icon: 'rss',
+    //     link: '#'
+    // },
+];
+const socials = "";
+export { socials }
--- js/main.js
+++ js/main.js
@@ -1,17 +1,39 @@
-// DUOMENYS
-const a = 10;
-const b = 100;
+import {renderSocials} from "./components/renderSocials.js";
+import {socials} from "./data/socialsData.js";
+
+renderSocials(socials);
 
 
 
 
-// LOGIKA
-function sum(a, b) {
-    return a + b;
-}
 
 
 
-// APJUNGIMAS
 
-console.log(sum(a, b));
+
+
+
+
+
+
+
+
+
+
+// // DUOMENYS
+// const a = 10;
+// const b = 100;
+
+
+
+
+// // LOGIKA
+// function sum(a, b) {
+//     return a + b;
+// }
+
+
+
+// // APJUNGIMAS
+
+// console.log(sum(a, b));
