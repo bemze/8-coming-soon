96fa96ee59c03368ced0a63c3cc80acfd99ac2f0
Mantas <bemze007@yahoo.com>
2020-12-01 13:19:33 +0200

    single prgressbar Validation and some JSDOC

---
+++ js/data/progressBarData.js
@@ -0,0 +1,19 @@
+const progressBarDate = [
+    {
+        selector: '.column.left',
+        title: 'Web development',
+        value: 90,
+    },
+    {
+        selector: '.column.left',
+        title: 'Web design',
+        value: 70,
+    },
+    {
+        selector: '.column.left',
+        title: 'UX',
+        value: 40,
+    },
+];
+
+export { progressBarDate }
